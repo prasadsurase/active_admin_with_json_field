da69212d5120e3044e9cdd485753e6b90c05f1d6
Prasad Surase <prasadsurase@gmail.com>
2016-04-19 23:23:18 +0530

    Added working ransackers for json keys

--- app/models/box.rb
+++ app/models/box.rb
@@ -1,3 +1,88 @@
 class Box < ActiveRecord::Base
   belongs_to :user
+=begin
+  ransacker :length_equals, formatter: -> (value){
+    binding.pry
+    data = where("dimensions ->> 'length' = '#{value}'").pluck :id
+    data = data.present? ? data : nil
+  } do |parent|
+    parent.table[:id]
+  end
+
+  ransacker :length_less_than, formatter: -> (value){
+    binding.pry
+    data = where("dimensions ->> 'length' < '#{value}'").pluck :id
+    data = data.present? ? data : nil
+  } do |parent|
+    parent.table[:id]
+  end
+
+  ransacker :length_greater_than, formatter: -> (value){
+    binding.pry
+    data = where("dimensions ->> 'length' > '#{value}'").pluck :id
+    data = data.present? ? data : nil
+  } do |parent|
+    parent.table[:id]
+  end
+
+  ransacker :breadth_equals, formatter: -> (value){
+    binding.pry
+    data = where("dimensions ->> 'breadth' = '#{value}'").pluck :id
+    data = data.present? ? data : nil
+  } do |parent|
+    parent.table[:id]
+  end
+
+  ransacker :breadth_less_than, formatter: -> (value){
+    binding.pry
+    data = where("dimensions ->> 'breadth' < '#{value}'").pluck :id
+    data = data.present? ? data : nil
+  } do |parent|
+    parent.table[:id]
+  end
+
+  ransacker :breadth_greater_than, formatter: -> (value){
+    binding.pry
+    data = where("dimensions ->> 'breadth' > '#{value}'").pluck :id
+    data = data.present? ? data : nil
+  } do |parent|
+    parent.table[:id]
+  end
+
+  ransacker :height_equals, formatter: -> (value){
+    binding.pry
+    data = where("dimensions ->> 'height' = '#{value}'").pluck :id
+    data = data.present? ? data : nil
+  } do |parent|
+    parent.table[:id]
+  end
+
+  ransacker :height_less_than, formatter: -> (value){
+    binding.pry
+    data = where("dimensions ->> 'height' < '#{value}'").pluck :id
+    data = data.present? ? data : nil
+  } do |parent|
+    parent.table[:id]
+  end
+
+  ransacker :height_greater_than, formatter: -> (value){
+    binding.pry
+    data = where("dimensions ->> 'height' > '#{value}'").pluck :id
+    data = data.present? ? data : nil
+  } do |parent|
+    parent.table[:id]
+  end
+=end
+
+  ransacker :length do |parent|
+    Arel::Nodes::InfixOperation.new('->>', parent.table[:dimensions], Arel::Nodes.build_quoted('length'))
+  end
+
+  ransacker :breadth do |parent|
+    Arel::Nodes::InfixOperation.new('->>', parent.table[:dimensions], Arel::Nodes.build_quoted('breadth'))
+  end
+
+  ransacker :height do |parent|
+    Arel::Nodes::InfixOperation.new('->>', parent.table[:dimensions], Arel::Nodes.build_quoted('height'))
+  end
 end
